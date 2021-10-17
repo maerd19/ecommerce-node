31019b7891ff94c46189234c958b23a980cd4b14
Alejandro Hernandez <alejandrohernang@gmail.com>
2021-10-17 11:54:15 -0500

    Related Products

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,8 @@ const {
     read,
     remove,
     update,
-    list
+    list,
+    listRelated
 } = require('../controllers/product')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
@@ -36,6 +37,7 @@ router.put(
 )
 
 router.get('/products', list )
+router.get('/products/related/:productId', listRelated)
 
 router.param('userId', userById)
 router.param('productId', productById)
